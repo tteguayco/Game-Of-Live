26b0366554c5d5ed871c1f5cd8baaa432fbe3608
tteguayco <teguayco95@gmail.com>
2017-08-18 21:31:35 +0100

    Painting alive cells after a mouse's click on the environment

--- src/gameoflife/Environment.java
+++ src/gameoflife/Environment.java
@@ -2,11 +2,15 @@ package gameoflife;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class Environment extends JPanel {
 	private static final Color BACKGROUND_COLOR = Color.BLACK;
+	private static final Color ALIVE_CELL_COLOR = Color.BLUE;
+	private static final Color DEAD_CELL_COLOR = Color.WHITE;
 	
 	private static final double ENVIRONMENT_WIDTH = 80;
 	private static final double ENVIRONMENT_HEIGHT = 50;
@@ -17,7 +21,7 @@ public class Environment extends JPanel {
 	private CellState[][] cells;
 	
 	public Environment() {
-		
+		addMouseListener(new CellsListener());
 	}
 	
 	public void setUpEnviroment() {
@@ -29,9 +33,6 @@ public class Environment extends JPanel {
 	private void calculateCellDimensions() {
 		cellWidth = getWidth() / ENVIRONMENT_WIDTH;
 		cellHeight = getHeight() / ENVIRONMENT_HEIGHT;
-		
-		//System.out.println(cellWidth);
-		//System.out.println(cellHeight);
 	}
 	
 	private void initializeCells() {
@@ -45,6 +46,23 @@ public class Environment extends JPanel {
 	}
 	
 	protected void paintComponent(Graphics g) {
+		
+		// Alive cells
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				if (cells[i][j] == CellState.ALIVE) {
+					g.setColor(ALIVE_CELL_COLOR);
+				} else {
+					g.setColor(DEAD_CELL_COLOR);
+				}
+				
+				g.fillRect((int) (j * cellWidth), 
+						(int) (i * cellHeight), 
+						(int) (cellWidth) + 1, 
+						(int) (cellHeight) + 1);
+			}
+		}
+		
 		g.setColor(Color.BLACK);
 		
 		// Horizontal lines
@@ -63,7 +81,28 @@ public class Environment extends JPanel {
 					(int) (ENVIRONMENT_WIDTH * cellHeight));
 		}
 	}
-
+	
+	private void changeCellState(int i, int j) {
+		if (cells[i][j] == CellState.ALIVE) {
+			cells[i][j] = CellState.DEAD;
+		} 
+		
+		else if (cells[i][j] == CellState.DEAD) {
+			cells[i][j] = CellState.ALIVE;	
+		}
+	}
+	
+	private class CellsListener extends MouseAdapter {
+		@Override
+	    public void mouseClicked(MouseEvent e) {
+			int cellRow = (int) (e.getY() / cellHeight);
+			int cellCol = (int) (e.getX() / cellWidth);
+			
+			changeCellState(cellRow, cellCol);
+			repaint();
+	    }
+	}
+	
 	public double getCellWidth() {
 		return cellWidth;
 	}
